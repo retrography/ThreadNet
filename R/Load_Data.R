b00cdba763feb531ab62167b92a269a568e462dd
Ezra Brooks <brooksez@msu.edu>
2018-05-17 09:33:35 -0400

    Add stub for data load functions

---
+++ R/Load_Data.R
@@ -0,0 +1,11 @@
+# Functions relating to loading a raw dataset
+
+# main function that gets called by server.R
+
+# Select a File
+
+# Run various validations
+	# return to main page if any conditions not met
+	# design to allow future additions of other validations
+
+# if file is good, return a dataset to app 
