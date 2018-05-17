6c5dabf43f6c2bf7bbc9815d05dcb038809a5428
Ezra Brooks <brooksez@msu.edu>
2018-05-17 10:17:56 -0400

    Initial setup for new ReadData functions

--- R/server/readData.R
+++ R/server/readData.R
@@ -1,33 +1,31 @@
-# Server Output Functions for Read Data Tab
+# Server: Read Data tab functions
 
-output$Data_Tab_Controls_1 <- renderUI({
+# limit what files to accept on input
+fileTypes <- c("text/csv","text/comma-separated-values,text/plain",".csv")
+
+# file selector dialog
+output$fileSelector <- renderUI({
 	tags$div(
 		align = "center",
-		fileInput(
-			"file1",
-			"Please select a .csv file",
-			accept = c(
-				"text/csv",
-				"text/comma-separated-values,text/plain",
-				".csv"
-			)
-		) 
+		fileInput("inputFile","Please select a .csv file",accept=fileTypes)
 	)
 })
 
-output$Data_Tab_Controls_2 <- renderUI({
+# user selects columns to include
+output$columnSelector <- renderUI({
 	checkboxGroupInput(
 		"CFcolumnsID",
 		"Select columns to include in analysis:",
+		# TODO: better way than "cfnames" but need to address occ() first
 		cfnames(occ()),
 		selected = cfnames(occ()),
 		inline = TRUE
 	)
 })
 
-
-output$Data_Tab_Output_2 <- DT::renderDataTable({
-	selectOcc() },
+# user filters data for review3
+output$dataFilter <- DT::renderDataTable(
+	selectOcc(), # TODO: this calls occ() -- review
 	filter = "top",
 	options = list(autoWidth = TRUE)
 )
